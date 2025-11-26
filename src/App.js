ce8f61acba14ef4dd2cb5ae36419da2c02d92f40
Michał Kowalczyk <michalkowal3k@gmail.com>
2025-11-26 23:38:38 +0100

    HashRouter instead of BrowserRouter and main path added in routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { HashRouter, Link, Routes, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks.js";
 import Author from "./features/author/Author";
 
 export const App = () =>
 (
-    <BrowserRouter>
+    <HashRouter>
         <nav>
             <ul>
                 <li>
@@ -18,7 +18,8 @@ export const App = () =>
             <Routes>
                 <Route path="/zadania" element={<Tasks />} />
                 <Route path="/autor" element={<Author />} />
+                <Route path="/" element={<Author />} />
             </Routes>
         </nav>
-    </BrowserRouter>
+    </HashRouter>
 )
