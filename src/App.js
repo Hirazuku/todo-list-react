5f6d96276c4afb33ac4658b28f03d6659c27d6d2
Michał Kowalczyk <michalkowal3k@gmail.com>
2025-11-26 22:51:15 +0100

    react-router installed and applied

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks.js";
 
-export const App = () => <Tasks />;
+export const App = () =>
+(
+    <BrowserRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">Zadania</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/zadania" element={<Tasks />} />
+            </Routes>
+        </nav>
+    </BrowserRouter>
+)
